5ebdf76e5c3a28efc6b67ded99446d3b08f92c16
HanYouyang <hanyouyang93@gmail.com>
2019-06-18 11:27:25 +0800

    上传

--- b-xjm/1/1.js
+++ b-xjm/1/1.js
@@ -1,14 +1,13 @@
+var log = console.log.bind(console)
 
-    var log = console.log.bind(console)
+var canvas = document.querySelector('#id-canvas')
+var context = canvas.getContext('2d')
 
-    var canvas = document.querySelector('#id-canvas')
-    var context = canvas.getContext('2d')
-    
-    var img = new Image()
-    img.src = 'paddle.png'
-    log(img)
-    //建立之后自动调用onload，所以必须写这里面运行
-    img.onload = function(){
-        context.drawImage(img, 0, 0)
-    }
+var img = new Image()
+img.src = 'paddle.png'//注意图片格式可能出现各种内容
+log(img)
+//建立之后自动调用onload，所以必须写这里面运行
+img.onload = function(){
+    context.drawImage(img, 0, 0)
+}
     
