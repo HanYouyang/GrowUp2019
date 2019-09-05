02ee05b6df7a9c37d6cc66bf31b7a6c79237d4f4
HanYouyang <hanyouyang93@gmail.com>
2019-09-05 19:21:31 +0800

    上传9

---
+++ Code/b-xjm/7to9打飞机/9/guagame/gua_image.js
@@ -0,0 +1,28 @@
+class GuaImage {
+    constructor(game, name){
+        this.game = game//传到手上
+        this.texture = game.textureByName(name)
+        this.x = 0
+        this.y = 0
+        this.w = this.texture.width
+        this.h = this.texture.height
+    }
+    static new(game, name){
+        var i = new this(game, name)
+        return i
+    }
+
+    draw(){
+        
+    }
+
+    update(){
+        
+    }
+}
+//不应该继承，但是暂时这么做吧
+// class Player extends GuaImage {
+//     constructor(game, name){
+//         super(game, name)
+//     }
+// }
