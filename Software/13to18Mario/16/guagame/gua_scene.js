a1f01391702cb144e67e476a3479fd0d3d17d6c4
HanYouyang <hanyouyang93@gmail.com>
2019-09-28 10:54:13 +0800

    上传15和开始的16

---
+++ Software/13to18Mario/16/guagame/gua_scene.js
@@ -0,0 +1,37 @@
+class GuaScene {
+    constructor(game){
+        //在这里给出接口就好了，别的不用管了
+        this.game = game
+        this.elements = []
+        this.debugModeEnabled = true
+    }
+    static new(game){
+        var i = new this(game)
+        return i
+    }
+
+    addElement(guaImage){
+        guaImage.scene = this
+        this.elements.push(guaImage)
+    }
+
+    draw(){//这里一定要被继承的函数必须有这个相应的内容
+        for (var element of this.elements) {
+            element.draw()
+        }
+    }
+
+    update(){
+        if (this.debugModeEnabled) {
+            for (var i = 0; i < this.elements.length; i++) {
+                var element = this.elements[i]
+                //log('element', element)
+                element.debug && element.debug()
+            }
+        }
+        for (var i = 0; i < this.elements.length; i++) {
+            var element = this.elements[i]
+            element.update()
+        }
+    }
+}
